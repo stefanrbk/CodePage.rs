5642bb0abc03e3c8d3bcf4f9119ae3a772cda26a
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-10-24 06:46:09 -0500

    Implement "DOS" code page (CP437)

---
+++ src/dos/mod.rs
@@ -0,0 +1,7 @@
+mod cp_437;
+
+pub use cp_437::Dos437;
+pub type CP437 = Dos437;
+
+pub use cp_437::Dos437Graphical;
+pub type CP437Graphical = Dos437Graphical;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 mod code_page;
+pub mod dos;
 pub mod windows;
 
 pub use code_page::CodePage;
 
 pub type ANSI = windows::Windows1252;
+pub type DOS = dos::Dos437;
+
+pub use dos::{CP437Graphical, CP437};
+pub use windows::CP1252;
