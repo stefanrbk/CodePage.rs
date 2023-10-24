5bcbb1d705fc4f9c095339ce57fb9ae27876e7a4
Stefan Kewatt <stefan.kewatt@gmail.com>
2023-10-24 06:01:47 -0500

    Implement "ANSI" code page (Windows1252)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 mod code_page;
+pub mod windows;
 
 pub use code_page::CodePage;
+
+pub type ANSI = windows::Windows1252;
---
+++ src/windows/mod.rs
@@ -0,0 +1,4 @@
+mod cp_1252;
+
+pub use cp_1252::Windows1252;
+pub type CP1252 = Windows1252;
